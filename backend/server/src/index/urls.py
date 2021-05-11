e6a11f97eb9790e8ee206c16b7899c58ca7ce978
whistlepark <arhanna@ucsd.edu>
2021-05-10 19:11:04 -0700

    index page

--- backend/server/src/index/urls.py
+++ backend/server/src/index/urls.py
@@ -15,8 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from index import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', include('index.urls')),
+    path('', views.index, name='index'),
 ]
