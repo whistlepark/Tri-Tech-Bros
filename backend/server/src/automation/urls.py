6888b94aa67a42f16edcb1a1427274d5fb00369b
whistlepark <arhanna@ucsd.edu>
2021-05-10 20:27:34 -0700

    automation page

--- backend/server/src/automation/urls.py
+++ backend/server/src/automation/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from automation import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', include('index.urls')),
-    path('automation/', include('automation.urls')),
+    path('', views.automation, name='automation'),
 ]
