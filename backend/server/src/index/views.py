378c250216e9b3203f7180f6903e250659cf0dd6
sepehr.fard <sepehr.fard@gmail.com>
2021-05-17 19:02:47 -0700

    added new changes

--- backend/server/src/index/views.py
+++ backend/server/src/index/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, StreamingHttpResponse
 from django.shortcuts import render
+
+from camera.cameraUtil import gen, VideoCamera
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html',context={})
+
+
+def videoFeed(request):
+    return StreamingHttpResponse(lambda r: gen(VideoCamera()),content_type='multipart/x-mixed-replace; boundary=frame')
