aa062c91611eb005204939b26d86a4cf08b3585a
whistlepark <arhanna@ucsd.edu>
2021-05-23 15:21:32 -0700

    updates

--- backend/server/src/src/urls.py
+++ backend/server/src/src/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('', include('index.urls')), 
     path('admin/', admin.site.urls),
     path('index/', include('index.urls')),
     path('automation/', include('automation.urls')),
+    path('logout', LogoutView.as_view()),
 ]
