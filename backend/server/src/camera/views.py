d4e177bd1dd4f138488a7c8f81c670502a4fac84
whistlepark <arhanna@ucsd.edu>
2021-05-30 12:11:53 -0700

    cam feed on devices page

--- backend/server/src/camera/views.py
+++ backend/server/src/camera/views.py
@@ -19,7 +19,7 @@ def devices(request,device_num=0):
 
 def video_feed(request,pk):
     camObj = IPCamera.objects.get(pk=pk)
-    return HttpResponse(json.dumps({'IP':camObj.IP,'Location':camObj.location}))
+    #return HttpResponse(json.dumps({'IP':camObj.IP,'Location':camObj.location}))
     return StreamingHttpResponse(camera.gen_frames(pk),content_type='multipart/x-mixed-replace; boundary=frame')
 
 def record(request,pk):
