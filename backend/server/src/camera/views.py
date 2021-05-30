6d7da509acd8a084a780dd61879d88d761053602
bfgustaf <bfgustaf@ucsd.edu>
2021-05-30 13:57:19 -0700

    views changes for streaming/record with threads

--- backend/server/src/camera/views.py
+++ backend/server/src/camera/views.py
@@ -20,14 +20,19 @@ def devices(request,device_num=0):
 def video_feed(request,pk):
     camObj = IPCamera.objects.get(pk=pk)
     #return HttpResponse(json.dumps({'IP':camObj.IP,'Location':camObj.location}))
-    return StreamingHttpResponse(camera.gen_frames(pk),content_type='multipart/x-mixed-replace; boundary=frame')
+    return StreamingHttpResponse(camera.start_thread(pk),content_type='multipart/x-mixed-replace; boundary=frame')
 
 def record(request,pk):
     camObj = IPCamera.objects.get(pk=pk)
-    return HttpResponse(camera.gen_frames(pk,record=True), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return HttpResponse(camera.start_record(pk), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+def stop_record(request, pk):
+    camera.stop_record(pk)
+    return HttpResponse()
 
 def remove(request,pk):
     if(request.method == "POST"):
+        camera.stop_thread(pk)
         IPCamera.objects.filter(pk=pk).delete()
     return HttpResponsePermanentRedirect('/devices')
 
