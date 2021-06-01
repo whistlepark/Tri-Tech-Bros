099c99e90a116b3f6ee63aa33ce8607b105f150d
sepehr.fard <sepehr.fard@gmail.com>
2021-05-31 19:55:22 -0700

    fixed css

---
+++ backend/server/src/camera/static/camera/js/devices.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+function onLoad(){
+	var devicesForm = document.getElementById('addDevice');
+	var canBtn = document.getElementById('cancel');
+	var addDeviceBtn = document.getElementById('addDeviceBtn');
+	var subBtn = document.getElementById('submit');
+
+	addDeviceBtn.addEventListener('click', function (e) {
+		console.log(devicesForm)
+		devicesForm.showModal();
+	})
+
+	canBtn.addEventListener('click', function (e) {
+		devicesForm.close();
+	});
+
+	subBtn.addEventListener('click', function (e) {
+		devicesForm.close();
+	});
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+	onLoad();
+});
