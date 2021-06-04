af19dd16259a8ac63b44bb124ce71aa3893312cb
bfgustaf <bfgustaf@ucsd.edu>
2021-06-04 09:55:47 -0700

    settings page

--- backend/server/src/settings/static/settings/js/devices.js
+++ backend/server/src/settings/static/settings/js/devices.js
@@ -28,3 +28,47 @@ function onLoad(){
 document.addEventListener('DOMContentLoaded', function(){
 	onLoad();
 });
+
+let modalBtns = [...document.querySelectorAll("button.edit_button")];
+modalBtns.forEach(function(btn) {
+	console.log
+  btn.onclick = function() {
+	let popup = btn.getAttribute('popup');
+	document.getElementById(popup)
+	  .style.display = "block";
+  }
+});
+let closeBtns = [...document.querySelectorAll(".close")];
+closeBtns.forEach(function(btn) {
+  btn.onclick = function() {
+	let popup_form = btn.closest('.popup_form');
+	popup_form.style.display = "none";
+  }
+});
+window.onclick = function(event) {
+  if(event.target.className === "popup_form") {
+	event.target.style.display = "none";
+  }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+var slider = document.getElementById("myRange");
+var output = document.getElementById("sliderval");
+slider.oninput = function() {
+	const response = fetch('/'+slider.name+slider.value, {
+method: 'POST',
+body: output,
+}
+);
+}
+});
+
+function post(id){
+var url = id;
+console.log(url);
+fetch(url).then(function(response) {
+  response.text().then(function(text) {
+	  console.log(text);
+  });
+});
+}
